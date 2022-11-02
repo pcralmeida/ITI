ddecce56a50eb9429fc50df6c90416d1c844ed5d
pcralmeida <pcca24102002@gmail.com>
2022-11-02 19:11:44 +0000

    Added session services

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/deiwed/main/DeiwedController.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/deiwed/main/DeiwedController.java
@@ -13,11 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pt.ulisboa.tecnico.rnl.dei.deiwed.main.dto.AttendeeDto;
 import pt.ulisboa.tecnico.rnl.dei.deiwed.main.service.AttendeeService;
+import pt.ulisboa.tecnico.rnl.dei.deiwed.main.dto.SessionDto;
+import pt.ulisboa.tecnico.rnl.dei.deiwed.main.service.SessionService;
 
 @RestController
 public class DeiwedController {
+
 	@Autowired
 	private AttendeeService attendeeService;
+
+	@Autowired
+	private SessionService sessionService;
 
 	@GetMapping("/attendees")
 	public List<AttendeeDto> getAttendees() {
@@ -43,4 +49,40 @@ public class DeiwedController {
 	public void deleteAttendee(@PathVariable long id) {
 		attendeeService.deleteAttendee(id);
 	}
+
+	@GetMapping("/sessions")
+	public List<SessionDto> getSessions() {
+		return sessionService.getAllSessions();
+	}
+
+	@PostMapping("/sessions")
+	public SessionDto createSession(@RequestBody SessionDto sessionDto) {
+		return sessionService.createSession(sessionDto);
+	}
+
+	@GetMapping("/sessions/{id}")
+	public SessionDto getSession(@PathVariable long id) {
+		return sessionService.getSession(id);
+	}
+
+	@PutMapping("/sessions/{id}")
+	public SessionDto updateSession(@PathVariable long id, @RequestBody SessionDto sessionDto) {
+		return sessionService.updateSession(id, sessionDto);
+	}
+
+	@DeleteMapping("/sessions/{id}")
+	public void deleteSession(@PathVariable long id) {
+		sessionService.deleteSession(id);
+	}
+
+	/*@GetMapping("/dishes")
+	public List<SessionDto> getDishes() {
+		return dishService.getAllSessions;
+	}
+
+	@GetMapping("/dishes/{id}")
+	public SessionDto getDish(@PathVariable long id) {
+		return dishService.getSession(id);
+	}
+*/ //FIXME: uncomment this line
 }
