8b83406fc8df742e56c265a4cace68a8ce5d93f7
pcralmeida <pcca24102002@gmail.com>
2022-11-02 21:02:29 +0000

    updated error message

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/deiwed/exceptions/ErrorMessage.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/deiwed/exceptions/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
 	NO_SUCH_ATTENDEE("Não existe nenhum participante com o ID %s", 1001),
 	ATTENDEE_NAME_NOT_VALID("O nome de participante especificado não é válido.", 1002),
 	NO_SUCH_SESSION("Não existe nenhuma sessão com o ID %s", 2001),
-	SESSION_NAME_NOT_VALID("O nome de sessão especificado não é válido.", 2002);
+	SESSION_NAME_NOT_VALID("O nome de sessão especificado não é válido.", 2002),
+	NO_SUCH_DISH("Não existe nenhum prato com o ID %s", 3001),
+	DISH_NAME_NOT_VALID("O nome de prato especificado não é válido.", 3002);
 
 
 	private final String label;
