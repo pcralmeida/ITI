43936a54120917e7b279f11311e65d0bacd97993
pcralmeida <pcca24102002@gmail.com>
2022-11-02 19:14:24 +0000

    Addded DishDto.ts

---
+++ src/frontend/src/models/deiwed/DishDto.ts
@@ -0,0 +1,8 @@
+export default class DishDto {
+    id: number = 0;
+    name: string = '';
+    description: string = '';
+    unitPrice: number = 0;
+    unitWeight: number = 0;
+    vegetarian: boolean = false;
+  }
