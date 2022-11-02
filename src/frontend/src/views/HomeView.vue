771e676ce546d315c52b1ebab9770bb0b4376b5f
pcralmeida <pcca24102002@gmail.com>
2022-11-02 22:20:36 +0000

    Changed background of Home view

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
-  <p>TODO: Home page for {{ appName }}</p>
+    <div class="home">
+    </div>  
 </template>
 
 <script lang="ts">
@@ -10,3 +11,12 @@ export default class HomeView extends Vue {
   appName: string = process.env.VUE_APP_NAME;
 }
 </script>
+
+<style scoped>
+  .home {
+    background-color: #009FE3;
+    background-size: cover;
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
