68c0f7af8dd4d273b25486f4286791353adf6486
pcralmeida <pcca24102002@gmail.com>
2022-11-03 10:19:52 +0000

    Adapted DeiwedApplication to add Atendees to the db

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/deiwed/DeiwedApplication.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/deiwed/DeiwedApplication.java
@@ -1,17 +1,31 @@
 package pt.ulisboa.tecnico.rnl.dei.deiwed;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import pt.ulisboa.tecnico.rnl.dei.deiwed.main.repository.AttendeeRepository;
+import pt.ulisboa.tecnico.rnl.dei.deiwed.main.entity.Attendee;;
+
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
-public class DeiwedApplication {
+public class DeiwedApplication implements CommandLineRunner {
+
+	@Autowired
+	AttendeeRepository attendeeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeiwedApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Attendee attendee = new Attendee("João", "ist199832", Attendee.ATTENDEE_TYPE.TEACHER, "joao@email.com", false, "none", "none"); 
+		attendeeRepository.save(attendee); //FIXME: clean when done
 	}
 
 }
