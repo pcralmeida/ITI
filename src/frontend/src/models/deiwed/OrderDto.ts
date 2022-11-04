df553ed52327a223fa956ed9036b21d03642ef33
pcralmeida <pcca24102002@gmail.com>
2022-11-04 21:31:48 +0000

    updated

--- src/frontend/src/models/deiwed/OrderDto.ts
+++ src/frontend/src/models/deiwed/OrderDto.ts
@@ -1,36 +1,4 @@
 export default class OrderDto {
-  date: string = '';
-
-  normalOption: {
-  id: number;
-  name: string;
-  description: string;
-  unitPrice: number;
-  unitWeight: number;
-  vegetarian: boolean;
-  } = {
-    id: 0,
-    name: '',
-    description: '',
-    unitPrice: 0,
-    unitWeight: 0,
-    vegetarian: false
-    };
-
-
-  vegetarianOption: {
-    id: number;
-    name: string;
-    description: string;
-    unitPrice: number;
-    unitWeight: number;
-    vegetarian: boolean;
-    } = {
-      id: 0,
-      name: '',
-      description: '',
-      unitPrice: 0,
-      unitWeight: 0,
-      vegetarian: false
-      };
+  normalOptionDishId: number = 0;
+  vegetarianOptionDishId: number = 0;
 }
