c575f6479db7cec979a0558c0af4b31e1c3568aa
pcralmeida <pcca24102002@gmail.com>
2022-11-04 21:31:36 +0000

    removed id

--- src/frontend/src/models/deiwed/DishDto.ts
+++ src/frontend/src/models/deiwed/DishDto.ts
@@ -1,8 +1,7 @@
 export default class DishDto {
-    id: number = 0;
     name: string = '';
     description: string = '';
     unitPrice: number = 0;
     unitWeight: number = 0;
     vegetarian: boolean = false;
-  }
+}
