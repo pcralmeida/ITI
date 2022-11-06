cca088b23d50106b40b9efd57302f72540258cc7
pcralmeida <pcca24102002@gmail.com>
2022-11-06 18:30:44 +0000

    styled landing page

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="home">
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center column pa-5>
+            <div class="display-2 font-weight-black white--text text-xs-center">Quartas-feiras</div>
+            <div class="ma-md-2 display-4 font-weight-black white--text text-xs-center mb-3">numa APP!</div>
+            <div class="ma-md-25 display-0 font-weight-bold white--text text-xs-center">Gere as tuas quartas com a nova app DEIWed.</div>
+        </v-layout>
+    </v-container>
     </div>  
 </template>
 
